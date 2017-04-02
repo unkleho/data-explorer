c0aa8b67b12434f5868e12594ca48126cb4cf297
phunkleho <kahoc@phm.gov.au>
2017-04-02 21:10:35 +1000

    Added env.PORT

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.prepare()
 
     app.render(req, res, '/data', params)
   })
-  .listen(3000, (err) => {
+  .listen(process.env.PORT || 3000, (err) => {
     if (err) throw err
     console.log('> Ready on http://localhost:3000')
   })
