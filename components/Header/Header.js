15a2ec8d948fc9cfd212bfbaecf227c98573c9bc
unkleho <kahoc@phm.gov.au>
2017-10-09 23:14:44 +1100

    Header redesign

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,6 +4,7 @@ import withRedux from 'next-redux-wrapper';
 
 import styles from './Header.css';
 import { initStore } from '../../store';
+import Link from '../../components/Link';
 
 class Header extends Component {
 
@@ -42,43 +43,39 @@ class Header extends Component {
 
     const {
       sources,
-      isMenuActive,
+      // isMenuActive,
     } = this.state;
 
     return (
-      <header className={`${isMenuActive && 'is-active'}`}>
+      <header>
 
         <div className="container">
 
           <div className="logo">
-            <span className="logo__abs">{sourceId}</span>
-              &nbsp;<span className="logo__text">Data Explorer</span>
-              &nbsp;<span style={{ fontSize: '0.6em', opacity: '0.7' }}>BETA</span>
+            {/* <span className="logo__abs">{sourceId}</span> */}
+            <span className="logo__text">Data Explorer</span>
+              &nbsp;<span style={{ fontSize: '0.5em', opacity: '0.7' }}>BETA</span>
               &nbsp;
-              <i
+              {/* <i
                 className="material-icons arrow"
                 onClick={this.handleMenuToggle}
-              >{isMenuActive ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}</i>
+              >{isMenuActive ? 'keyboard_arrow_up' : 'keyboard_arrow_down'}</i> */}
           </div>
 
-          <div className={`menu ${isMenuActive && 'is-active'}`}>
+          <div className="menu">
             <ul role="menu">
               {sources.map((s) => {
-                // const idParam = s.id === 'UNESCO' ? '&id=DEMO_DS' : '';
-
                 return (
                   <li
                     onClick={this.handleMenuToggle}
                     className={s.id === sourceId && 'is-active'}
                     key={s.id}
                   >
-                    {/* <Link
-                      href={`/data?sourceId=${s.id}${idParam}`}
-                    > */}
-                      <a
-                        href={`/data?sourceId=${s.id}`}
-                      >{s.title}</a>
-                    {/* </Link> */}
+                    <Link
+                      href={`/${s.slug}`}
+                    >
+                      <a>{s.title}</a>
+                    </Link>
                   </li>
                 )
               })}
--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,9 @@ const routes = require('next-routes')();
 
 routes
   .add('example-page', '/example-page/:id');
+  // .add('abs', '/abs')
+  // .add('oecd', '/oecd')
+  // .add('ukds', '/ukds')
+  // .add('unesco', '/unesco');
 
 module.exports = routes;
