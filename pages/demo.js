2402994ee3b0039a4e0454932a6666ad09a2a739
unkleho <kahoc@phm.gov.au>
2018-01-17 01:10:40 +1100

    Added prettier.

--- pages/demo.js
+++ pages/demo.js
@@ -10,47 +10,60 @@ import withData from '../lib/withData';
 // const dataSets = buildDataSets(dataSetsRaw);
 
 class Demo extends Component {
+	render() {
+		// console.log();
+		return (
+			<App url={this.props.url}>
+				<ul>
+					{this.props.organisations &&
+						this.props.organisations.map((organisation, i) => {
+							const orgId = organisation.organisationId.toLowerCase();
 
-  render() {
-    // console.log();
-    return (
-      <App url={this.props.url}>
-        <ul>
-          {this.props.organisations && this.props.organisations.map((organisation, i) => {
-            return (
-              <li key={`organisation-${i}`}>
-                <Link href={`/${organisation.organisationId.toLowerCase()}`} as={`/${organisation.organisationId.toLowerCase()}`}>
-                  <a>{organisation.title}</a>
-                </Link>
+							return (
+								<li key={`organisation-${i}`}>
+									<Link
+										href={`/${organisation.organisationId.toLowerCase()}`}
+										as={`/${organisation.organisationId.toLowerCase()}`}
+									>
+										<a>{organisation.title}</a>
+									</Link>
 
-                <ul>{organisation.dataSets.map(dataSet => (
-                  <li>{dataSet.title}</li>
-                ))}</ul>
-              </li>
-            )
-          })}
-        </ul>
-      </App>
-    );
-  }
-
+									<ul>
+										{organisation.dataSets.map((dataSet) => (
+											<li>
+												<Link href={`/${orgId}/${dataSet.originalId}`}>
+													<a>{dataSet.title}</a>
+												</Link>
+											</li>
+										))}
+									</ul>
+								</li>
+							);
+						})}
+				</ul>
+			</App>
+		);
+	}
 }
 
 const query = gql`
-  query {
-    organisations: allOrganisations {
-      organisationId
-      title
-      dataSets {
-        title
-      }
-    }
-  }
+	query {
+		organisations: allOrganisations {
+			organisationId
+			title
+			dataSets {
+				title
+				originalId
+			}
+		}
+	}
 `;
 
-export default withData(graphql(query, {
-  props: (({ data }) => {
-    // console.log(data);
-    return data;
-  }),
-})(Demo));
+export default withData(
+	graphql(query, {
+		props: ({ data }) => {
+			// console.log(data);
+			return data;
+		},
+	})(Demo),
+);
--- readme.md
+++ readme.md
@@ -11,8 +11,11 @@ $ now alias [url] dataexplorer.co
 # Get dataSet metadata from SDMX
 - `http://stat.data.abs.gov.au/sdmx-json/metadata/[id]/all`
 - `http://stats.oecd.org/sdmx-json/metadata/[id]/all`
-- `https://stats.ukdataservice.ac.uk/sdmx-json/[id]/all`
-- `http://data.uis.unesco.org/sdmx-json/[id]/all`
+- `https://stats.ukdataservice.ac.uk/sdmx-json/metadata/[id]/all`
+- `http://data.uis.unesco.org/sdmx-json/metadata/[id]/all`
+
+# Get dataSet data from SDMX
+- `http://stat.data.abs.gov.au/sdmx-json/data/[id]/[key]/all?detail=Full&dimensionAtObservation=AllDimensions`
 
 ## Based on nextjs-postcss branch
 https://github.com/almeynman/nextjs-postcss
