f82223396032dd2cc1e725370898c55d1338eaa9
unkleho <kahoc@phm.gov.au>
2018-05-30 11:29:32 +1000

    Readme update

---
+++ components/ChartDimensions/ChartDimensions.js
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+// import PropTypes from 'prop-types';
+
+import './ChartDimensions.css';
+
+class ChartDimensions extends Component {
+  // static propTypes = {};
+
+  render() {
+    // const {} = this.props;
+
+    return (
+      <div className="chart-dimensions">
+        <span></span>
+      </div>
+    )
+  }
+}
+
+export default ChartDimensions
